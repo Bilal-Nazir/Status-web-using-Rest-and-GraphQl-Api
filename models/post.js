3df8953d664df3ed8477ef7d391bf6677b42f88b
Bilal-Nazir <bilalnazir888@gmail.com>
2023-08-31 14:09:47 +0500

    MY first repo

---
+++ models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+
+    }
+}, {timestamps: true})   //this gives created at and updated at
+
+module.exports = mongoose.model(('post'), postSchema);
